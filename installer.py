e2e799aa651a4f91a56450efaa64ea89efef729f
dime35 <dime35.gd@gmail.com>
2021-03-20 12:57:18 -0400

    finished basic cve checker

--- installer.py
+++ installer.py
@@ -3,18 +3,9 @@ import os
 import subprocess
 
 
-#exein = "msfconsole -q -x 'db_status' >> log.txt"
-
-
-#exe = os.system(exein)
-#f = open("log.txt", "w")
-#print("hi")
-
-#os.system(bytes(exein))
-#if __name__ == '__main__':
 ip = "13.210.40.244" #input("enter your IP: ")
 exein = "msfconsole -q -x 'db_nmap -sV " + ip + " ; hosts ; services ; vulns ; quit'"
-#exein = "msfconsole -q -x 'db_status ; version ; quit'"
+#exein = "msfconsole -q -x 'db_status ; version ; exit'"
 os.system(exein)
 
 # msfconsole -x 'db_nmap -sV 13.210.40.244';'exit'
