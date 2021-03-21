949c2935ef89cc8608fa98f3fd3918c29bc2afe1
dime35 <dime35.gd@gmail.com>
2021-03-21 00:37:10 -0400

    Final Project

---
+++ logfilter.py
@@ -0,0 +1,51 @@
+import re
+
+filename = 'log.txt'
+
+def readhost():
+    with open(filename) as f:
+        content = f.readlines()
+        host = []
+        i = 0;
+        for n in content:
+            j = re.sub(r'\s+', ' ', n)
+            if j == ('host port proto name state info '):
+                i += 1
+            elif i == 1:
+                i += 1
+            elif i == 2 and j != ' ':
+                host.append(j)
+            elif i == 2:
+                i+=1
+    return host
+
+def readvuln():
+    with open(filename) as f:
+        content = f.readlines()
+        vuln = []
+        i = 0;
+        for n in content:
+            j = re.sub(r'\s+', ' ', n)
+            if n == 'Vulnerabilities ':
+                i += 1
+            elif i == 1:
+                i += 1
+            elif i == 2 and n != ' ':
+                vuln.append()
+            elif i == 2:
+                i += 1
+    return vuln
+
+
+def splithost(line):
+    strs = line.split()
+    return [strs[1], strs[3], strs[4]]
+
+
+def splitvuln(line):
+    strs = line.split()
+    return [strs[0], strs[1]]
+
+
+
+
--- mainui.py
+++ mainui.py
@@ -1,15 +1,18 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 import re
 import execute
+import logfilter
 
 ipregex = r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"
 
 root = tk.Tk()
-root.wm_title("really really funny")
+root.wm_minsize(350, 400)
+root.wm_title("Anti-Keypogger")
 
-v=tk.StringVar()
+v = tk.StringVar()
 
-title = title = tk.Label(text="IP")
+title = title = tk.Label(text="Enter YOUR IP")
 title.pack()
 
 entry = tk.Entry(text=v)
@@ -23,8 +26,32 @@ def buttonPressed():
     if search == None:
         v.set("")
     else:
+        open('log.txt', 'w').close()
         execute.exestart(input)
+        fillporttree()
 
+
+
+porttree = ttk.Treeview( columns=("Port", "Type"), height=10)
+
+porttree.heading('#0', text='Port')
+porttree.heading('#1', text='Type')
+porttree.heading('#2', text='Status')
+
+porttree.column('#0', stretch=tk.YES)
+porttree.column('#1', stretch=tk.YES)
+porttree.column('#2', stretch=tk.YES)
+
+porttree.pack(pady = 15)
+
+vulntree = ttk.Treeview( columns=("Vulnerability"), height=2)
+
+vulntree.heading('#0', text='Vulnerability')
+vulntree.heading('#1', text='Date')
+
+vulntree.column('#0', stretch=tk.YES)
+vulntree.column('#1', stretch=tk.YES)
+vulntree.pack(pady = 15)
 
 submit = tk.Button(text="Start scan",
                    command=buttonPressed,
@@ -32,5 +59,12 @@ submit = tk.Button(text="Start scan",
                    height=5,
                    bg="black",
                    fg="white")
+def fillporttree():
+    ports = logfilter.readhost();
+    for port in ports:
+        s = logfilter.splithost(port)
+        porttree.insert(parent='',index = 0,text = s[0], values=(s[1], s[2]))
+
+
 submit.pack()
 root.mainloop()
