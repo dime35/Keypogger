b6f6503a5496ae2250d2ba51cee1de976e65db3c
dime35 <dime35.gd@gmail.com>
2021-03-21 10:43:20 -0400

    Final Project

--- installer.py
+++ installer.py
@@ -1,13 +1,8 @@
-from urllib.request import urlretrieve
+
 import os
-import subprocess
 from sys import argv
 
-#"13.210.40.244"
 ip = argv[1]
-print(ip)
 exein = "msfconsole -q -x 'db_nmap -sV " + ip + " ; hosts ; services ; vulns ; quit'"
-#exein = "msfconsole -q -x 'db_status ; version ; exit'"
 os.system(exein)
-# msfconsole -x 'db_nmap -sV 13.210.40.244';'exit'
 
