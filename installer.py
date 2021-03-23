ba49e47a99cfcb67ff95b6221bf5230573ad84ea
Kyle Goulet <69086568+KyleGoulet@users.noreply.github.com>
2021-03-23 00:25:22 -0400

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,7 +1,8 @@
-
+#import os for interacting with the operating system
 import os
 from sys import argv
 
+#Starts metasploit with msfconsole and then runs a nmap scan. Then takes results to run with hosts, services, vulns, and then exits the terminal
 ip = argv[1]
 exein = "msfconsole -q -x 'db_nmap -sV " + ip + " ; hosts ; services ; vulns ; quit'"
 os.system(exein)
