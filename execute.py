328f7499265a498cd1c374b95024c6cecf1db6e7
Kyle Goulet <69086568+KyleGoulet@users.noreply.github.com>
2021-03-23 00:27:18 -0400

    Update execute.py

--- execute.py
+++ execute.py
@@ -1,3 +1,5 @@
 import os
+
+#Bash script embeded with python3 to send command terminal output to a .txt file
 def exestart(ip):
     os.system('bash -c "python3 installer.py ' + ip + ' > log.txt"')
